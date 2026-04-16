5aadfc09571e2de6ba0a85fb2d28a9dd415d7990
Khushi Chandel <khushichandel1512@gmail.com>
2026-04-16 15:04:39 +0530

    fix: update api url for production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api/v1', 
+  baseURL: 'https://primetrade-backend-intern-task.onrender.com', 
 });
 
 api.interceptors.request.use(
